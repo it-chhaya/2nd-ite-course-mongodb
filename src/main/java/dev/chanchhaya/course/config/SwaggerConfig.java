db84e3136188fcedc523f12c2088408b08c595b8
it.chhaya <it.chhaya@gmail.com>
2024-08-22 16:16:46 +0700

    update docker file

---
+++ src/main/java/dev/chanchhaya/course/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package dev.chanchhaya.course.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${course.openapi.dev-url}")
+    private String devUrl;
+
+    @Value("${course.openapi.prod-url}")
+    private String prodUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Server prodServer = new Server();
+        prodServer.setUrl(prodUrl);
+        prodServer.setDescription("Server URL in Production environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("it.chhaya@gmail.com");
+        contact.setName("Chan Chhaya");
+        contact.setUrl("https://www.istad.co");
+
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("Course Management API")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints to manage homework.").termsOfService("https://www.bezkoder.com/terms")
+                .license(mitLicense);
+
+        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+    }
+}
