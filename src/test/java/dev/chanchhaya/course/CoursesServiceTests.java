0cea575343deaee356bd6eb38fdb04c8ff24b285
it.chhaya <it.chhaya@gmail.com>
2024-09-18 14:34:37 +0700

    update test

--- src/test/java/dev/chanchhaya/course/CoursesServiceTests.java
+++ src/test/java/dev/chanchhaya/course/CoursesServiceTests.java
@@ -15,39 +15,6 @@ import java.util.List;
 @SpringBootTest
 class CoursesServiceTests {
 
-    @Autowired
-    private CourseRepository courseRepository;
 
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Test
-    void contextLoad() {
-
-    }
-
-    @Test
-    void filterCourses() {
-        Query query = new Query();
-        /*Criteria criteria = Criteria
-                .where("title")
-                .regex("20", "i");
-        Criteria criteria2 = Criteria
-                .where("price")
-                .lte(100);*/
-        Criteria byInstructorUsername = Criteria
-                .where("instructor.username")
-                .is("chhaya");
-        Criteria criteria3 = new Criteria();
-        criteria3.andOperator(byInstructorUsername);
-        query.addCriteria(criteria3);
-        List<Course> courses = mongoTemplate.find(query.with(PageRequest.of(0,1)), Course.class);
-        //List<Course> courses = mongoTemplate.query(Course.class).matching(query).all();
-        //List<Course> courses = courseRepository.filterCoursesByTitle("^A");
-        //List<Course> courses = courseRepository.findCoursesTitle("spring");
-        //List<Course> courses = courseRepository.findCoursesByPrice(BigDecimal.valueOf(100));
-        System.out.println(courses);
-        System.out.println(courses.size());
-    }
 
 }
